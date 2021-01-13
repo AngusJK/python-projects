4b084180f171e5c5b53ae2538a47e12bb49be8bd
Angus Kellett <anguskellett@hotmail.com>
2021-01-13 00:19:37 +0000

    initial commit

---
+++ app.py/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("Hello World")
+print("Everything you could ever ask for is here.")
+print('*' * 10)
+price = 43.99
+price = 0.78
+is_interesting = False
+print(price)
